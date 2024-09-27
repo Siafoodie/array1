fc12b15d2069d5006badf22bf1e354fe844ba9ce
Sadia islam usha <145834502+Siafoodie@users.noreply.github.com>
2024-09-28 00:10:19 +0600

    Add files via upload

--- qwe.c
+++ qwe.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+void f(int arr[], int size) {
+    printf("Duplicates in the array: ");
+    int fo = 0;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                printf("%d ", arr[i]);
+                fo = 1;
+                break; // To avoid printing the same duplicate again
             }
         }
     }
-}
 
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
+    if (!fo) {
+        printf("No duplicates found.");
     }
     printf("\n");
 }
 
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr[] = {1, 2, 3, 4, 5, 3, 2};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Original array: ");
-    printArray(arr, size);
-
-    bubbleSort(arr, size);
-
-    printf("Sorted array: ");
-    printArray(arr, size);
+    f(arr, size);
 
     return 0;
 }
+
+
 
