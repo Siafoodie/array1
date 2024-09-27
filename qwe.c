aee7ed3d77cda34e4d9fa8c75e61384239965824
Sadia islam usha <145834502+Siafoodie@users.noreply.github.com>
2024-09-28 00:07:04 +0600

    Add files via upload

--- qwe.c
+++ qwe.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 
-void rot(int arr[], int size) {
-    if (size <= 1) {
-        return; 
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-    
-    int l = arr[size - 1]; 
-    
-    for (int i = size - 1; i > 0; i--) {
-        arr[i] = arr[i - 1];
-    }
+}
 
-    arr[0] = l; // Place the last element at the first position
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     printf("Original array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    printArray(arr, size);
 
-    rot(arr, size);
+    bubbleSort(arr, size);
 
-    printf("Array after cyclic rotation: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    printf("Sorted array: ");
+    printArray(arr, size);
 
     return 0;
 }
+
